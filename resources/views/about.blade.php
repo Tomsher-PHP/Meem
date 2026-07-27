a5611c4aa296bbfe2b1126bf38c6a6d3a654d46c
SafvanTomsher <safvan.m@tomsher.com>
2026-07-27 18:21:57 +0400

    updates

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,7 @@
 @section('page_id', 'about-page')
 
 @section('content')
-<section class="about-hero"  style="background-image: url('{{ asset('assets/about-us-banner.webp') }}');">
+<section class="about-hero">
     <div class="about-hero-copy mx-auto px-5 pb-12 sm:px-10 sm:pb-16 lg:px-16 xl:px-20">
         <div class="max-w-[310px]">
             <h1 class="display-title hero-reveal text-[50px] leading-none">About Us</h1>
